f0a1dea9af1a6ab2caab7396e4e978f35dd5b530
Tushar Dattaguru Kadu <131634787+Tusharkadu1298@users.noreply.github.com>
2023-05-06 19:22:46 +0530

    Create sum_and_average_in_list.py

---
+++ sum_and_average_in_list.py
@@ -0,0 +1,7 @@
+ # 7.  Python Program to find sum and average of List in Python
+  
+list = [10,20,30,40,50,60,70,80,90,100]
+add = sum(list)
+print(add)
+average = sum(list)/len(list)
+print(average)
