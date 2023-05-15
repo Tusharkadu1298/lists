750ed1e12857a79ba08993f98150b59fcef42932
Tushar Dattaguru Kadu <131634787+Tusharkadu1298@users.noreply.github.com>
2023-05-15 18:18:28 +0530

    Update sum_and_average_in_list.py

--- sum_and_average_in_list.py
+++ sum_and_average_in_list.py
@@ -5,3 +5,18 @@ add = sum(list)
 print(add)
 average = sum(list)/len(list)
 print(average)
+
+
+# using for loop
+sum = 0
+for i in l:
+    sum += i
+print(sum)
+ 
+len = 0
+for i in l:
+    len += 1
+print(len)
+
+c = sum/len
+print(c)
